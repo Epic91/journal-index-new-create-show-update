833712c4d388d6ccd2944b700137d9def81520ae
Epic91 <hernandeze791@gmail.com>
2021-08-18 17:00:07 -0500

    used resources in routes

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,12 +1,19 @@
 class EntriesController < ApplicationController
   def index
-    
+    @entries = Entry.all
   end
 
   def new
   end
 
   def create
+    @entry = Entry.new
+    @entry[:name] = params[:entry][:name]
+    @entry[:subject] = params[:entry][:subject]
+    @entry[:body] = params[:entry][:body]
+    @entry.save
+
+    redirect_to 
   end
 
   def show
