485b7aeb1124148998785f2d88328c6be0f1241b
Epic91 <hernandeze791@gmail.com>
2021-08-18 17:00:20 -0500

    used resources in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,3 @@
 Rails.application.routes.draw do
-  get 'entries/index'
-  get 'entries/new'
-  get 'entries/create'
-  get 'entries/show'
-  get 'entries/edit'
-  get 'entries/update'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :entries, only: [:index, :new, :create, :show, :edit, :update]
 end
