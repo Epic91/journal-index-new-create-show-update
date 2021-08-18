5b22a66decb5c48233074c78964bbfc6e60dcb02
Epic91 <hernandeze791@gmail.com>
2021-08-18 16:57:12 -0500

    added methods

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'entries/index'
+  get 'entries/new'
+  get 'entries/create'
+  get 'entries/show'
+  get 'entries/edit'
+  get 'entries/update'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
